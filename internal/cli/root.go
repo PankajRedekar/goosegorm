1ae8ae074aa784f65b89d666e7f7690bd101f2de
Pankaj <ppaannkkaajj2033@gmail.com>
2025-11-06 11:56:20 +0530

    Add package versioning
    - Add version.go with Version constant (v0.1.0)
    - Add GetVersion() function for version queries
    - Add 'version' CLI command
    - Set rootCmd.Version for --version flag support
    - Create git tag v0.1.0 for initial release

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -1,6 +1,8 @@
 package cli
 
 import (
+	"fmt"
+	"github.com/pankajredekar/goosegorm"
 	"github.com/spf13/cobra"
 )
 
@@ -8,6 +10,20 @@ var rootCmd = &cobra.Command{
 	Use:   "goosegorm",
 	Short: "Django-style migration framework for GORM",
 	Long:  "GooseGORM is a Go-based migration framework for GORM with in-memory schema simulation",
+	Version: goosegorm.GetVersion(),
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number",
+	Long:  "Print the version number of GooseGORM",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("goosegorm version %s\n", goosegorm.GetVersion())
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
 }
 
 // Execute runs the CLI
---
+++ version.go
@@ -0,0 +1,9 @@
+package goosegorm
+
+// Version is the current version of GooseGORM
+const Version = "0.1.0"
+
+// GetVersion returns the current version of GooseGORM
+func GetVersion() string {
+	return Version
+}
